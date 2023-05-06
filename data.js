672ada2778d9487ed93589a33111050310f120e3
Saurabh Nagorao Radke <111894486+SaurabhRadke@users.noreply.github.com>
2023-05-06 23:50:50 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,758 @@
+var EmployeLocation={
+    type: "FeatureCollection",
+    "features": [
+      {
+        type: "Feature",
+        properties: {
+            Name:"John Smith",
+            Department:"Sales",
+            Addres:"Ambazari - Hingna Rd, Subhash Nagar, Nagpur, Maharashtra 440022"
+        },
+        geometry: {
+          coordinates: [
+            79.04368820882024,
+            21.123648211199217
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        properties: {
+            Name:"Jane Doe",
+            Department:"Marketing",
+            Addres:"Gorepeth, Nagpur, Maharashtra 440012"
+            },
+        geometry: {
+          coordinates: [
+            79.0817942406313,
+            21.149616315398205
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Michael Lee",
+            Department:"Finanace",
+            Addres:"Sai Mess, Mohan Nagar, Nagpur, Maharashtra 440001"
+        },
+        "geometry": {
+          "coordinates": [
+            79.08072446171619,
+            21.160934178830974
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Sarah Johnson",
+            Department:"Human Resource",
+            Addres:"Mangalwari, Mahal, Nagpur, Maharashtra 440032"
+        },
+        "geometry": {
+          "coordinates": [
+            79.10510457207442,
+            21.14100957347274
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"David Kim",
+            Department:"Sales",
+            Addres:"Sonegaon, Nagpur, Maharashtra 440005"
+        },
+        "geometry": {
+          "coordinates": [
+            79.06333780960927,
+            21.095754922268572
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Emily Chen",
+            Department:"Technical",
+            Addres:"Gandhinagar, Vayusena Nagar, Nagpur, Maharashtra 440007"
+        },
+        "geometry": {
+          "coordinates": [
+            79.01927881709172,
+            21.160463747408933
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Kevin Brown",
+            Department:"Finance",
+            Addres:"Wadi, Nagpur, Maharashtra"
+        },
+        "geometry": {
+          "coordinates": [
+            78.99469240341126,
+            21.134169919820057,
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Samantha Liu",
+            Department:"Human Resourse",
+            Addres:"Deepak Nagar, New Khasala, Maharashtra 440026"
+        },
+        "geometry": {
+          "coordinates": [
+            79.1166410152663,
+            21.191643395545217
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"James Lee",
+            Department:"Sales",
+            Addres:"Mohadi, Bhandara, 441909, Maharashtra, India"
+        },
+        "geometry": {
+          "coordinates": [
+            79.60360342073909,
+            21.313379345598065
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Ashley Smith",
+            Department:"Marketing",
+            Addres:"Hinduhridey Samrat M B T F, Padma Nagar, Bhiwandi, 421305, Maharashtra, India"
+        },
+        "geometry": {
+          "coordinates": [
+            73.06789371787289,
+            19.290470130991707
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Eric Chen",
+            Department:"Finance",
+            Addres:"Vada, Palghar, 421303, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            73.3235876329546,
+            19.686731299862473
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Matthew Brown",
+            Department:"Sales",
+            Addres:"16, Sahar Road, Vijay Nagar, Andheri East, Mumbai, 400069, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            72.8478560163372,
+            19.113684210484223
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Rachel Liu",
+            Department:"IT",
+            Addres:"Thane, 400705, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            73.04479573112042,
+            19.08360406271656
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Jason Lee",
+            Department:"Sales",
+            Addres:"Tadoba National Park, Chandrapur, 442404, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            79.36869076899643,
+            20.08819774558468
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Lauren Smith",
+            Department:"IT",
+            Addres:"BTS, Ballarpur, 442901, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            79.36869076899643,
+            19.843524543823634
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Ryan Brown",
+            Department:"IT",
+            Addres:"Chandrapur, 442505, Maharashtra, India"
+        },
+        "geometry": {
+          "coordinates": [
+            79.21993669782898,
+            19.922210872657914
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Amanda Liu",
+            Department:"Sales",
+            Addres:"Tadoba National Park, Chandrapur, 441224, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            79.52674196961215,
+            20.09692904102208
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Justine Lee",
+            Department:"IT",
+            Addres:"Bhokardan, Jalna, 431215, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            75.78929593152071,
+            20.079465963514878
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Tiffany Smith",
+            Department:"Finance",
+            Addres:"Badnapur, Jalna, 431202, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            75.77999880207253,
+            19.97464668535133
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Brian Chen",
+            Department:"Finance",
+            Addres:"Aurangabad, 431008, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            75.47232788405185,
+            20.01787149658398
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Karen Kim",
+            Department:"Sales",
+            Addres:"Eklehara Colony, Nashik, 422105, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            73.89016336871526,
+            19.97432496081734
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Jane Doe",
+            Department:"Finance",
+            Addres:"Dindori, Nashik, 422203, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            73.72429499283848,
+            20.137217158154286
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"John Smith",
+            Department:"Sales",
+            Addres:"Satpur, Nashik, 422007, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            73.71675552120752,
+            20.00975068076913
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Jonathan Johnson",
+            Department:"Finance",
+            Addres:"Aurangabad, 431007, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            75.48099188279889,
+            19.775794108083275
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Brandon Young",
+            Department:"Sales",
+            Addres:"Khuldabad, Aurangabad, 431101, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            75.2698866771382,
+            19.99558134909158
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Maria Garcia",
+            Department:"Human Resource",
+            Addres:"Gangapur, Aurangabad, 431133, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            75.17187354593801,
+            19.853818020774398
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Daniel Rodriguez",
+            Department:"Human Resource",
+            Addres:"Aurangabad, 431008, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            75.33774192181477,
+            19.98849620500704
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Abhishek Agarwal",
+            Department:"Human Resource",
+            Addres:"Charholi Budruk, Pimpri Chinchwad, 412105, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            73.8977028403462,
+            18.65093234459775
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Tanvi Khatri",
+            Department:"Marketing",
+            Addres:"Kondhwa Khurd, Pune, 411048, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            73.89016336871526,
+            18.479401213862502
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:" Tanya Ahuja",
+            Department:"Human Resource",
+            Addres:"Haveli, Pune, 412207, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            74.0334133296993,
+            18.550893474064708
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Varsha Laghari",
+            Department:"Marketing",
+            Addres:"Haveli, Pune, 412201, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            74.01079491480661,
+            18.457947715467682
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Neha Patel",
+            Department:"Sales",
+            Addres:"Mulshi, Pune, 412115, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            73.66397921979274,
+            18.56518833843056
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"John Smith",
+            Department:"Sales",
+            Addres:"Kothrud, Pune, 411021, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            73.76953182262233,
+            18.49370205505126
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Aditya Patel",
+            Department:"Marketing",
+            Addres:"Khed, Pune, 410505, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            73.73937393610032,
+            18.865102600843713
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Deepak Bakshil ",
+            Department:"Sales",
+            Addres:"Kedgaon, Ahmednagar, 414005, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            74.69688683320337,
+            19.050495929033403
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Aryan Babu",
+            Department:"Marketing",
+            Addres:"E Ridge Road, DMR Area, Ahmednagar, 414002, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            74.7873604927712,
+            19.07899965691645
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Rakesh Balakrishnan",
+            Department:"Sales",
+            Addres:"Nagar, Ahmednagar, 414006, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            74.72704471972554,
+            19.01485938288272
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Ashish Reddy",
+            Department:"Finance",
+            Addres:"Badnapur, Jalna, 431215, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            75.6167023721521,
+            20.038085515531364
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Pranav Banerjee",
+            Department:"IT",
+            Addres:"Khed, Pune, 410501, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            73.76953182262233,
+            18.815153880244992
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"ABHI Basu",
+            Department:"Data Security",
+            Addres:"Chikhali, Pimpri Chinchwad, 411062, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            73.80242119097147,
+            18.681630930523724
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Ajit Bedi",
+            Department:"Finance",
+            Addres:"Paithan, Aurangabad, 431107, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            75.41182106653983,
+            19.44449167491163
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Prateek Varma",
+            Department:"Human Resource",
+            Addres:"Yevla, Nashik, 423401, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            74.50744530564702,
+            20.02572260062719
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Raghav Dara",
+            Department:"IT",
+            Addres:"Vaijapur, Aurangabad, 431116, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            74.73597036350662,
+            19.98460289106211
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Isha Smith",
+            Department:"Cyber Security",
+            Addres:"Vaijapur, Aurangabad, 423703, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            74.77973048096828,
+            19.906903054685074
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Jay Chabra",
+            Department:"Marketing",
+            Addres:"Vaijapur, Aurangabad, 423701, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            74.67276130495006,
+            19.920617568132116
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"John Smith",
+            Department:"FINANCE",
+            Addres:"Kopargaon, Ahmednagar, 423601, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            74.4490984823623,
+            19.929759916265056
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Kumar Smith",
+            Department:"IT",
+            Addres:"Washim, 444505, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            77.21743391062597,
+            20.11790007607017
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"John Smith",
+            Department:"Sales",
+            Addres:"Yavatmal, 445001, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            78.10279540774411,
+            20.384888313736724
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Neeraj Dara ",
+            Department:"Human Resource",
+            Addres:"Akola, 444006, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            76.98818852297876,
+            20.6366201931002
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"John Smith",
+            Department:"Finance",
+            Addres:"Washim, 444505, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            77.07514367001755,
+            20.132744778512617
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Raghav Chawla",
+            Department:"Marketing",
+            Addres:"Washim, 444505, Maharashtra, India"},
+        "geometry": {
+          "coordinates": [
+            76.56922281452086,
+            20.70318566729246
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Jonhathan Michel",
+            Department:"IT",
+            Addres:"Barshitakli 444002, Maharashtra"},
+        "geometry": {
+          "coordinates": [
+            77.05142862991568,
+            20.59222701807313
+          ],
+          type: "Point"
+        }
+      },
+      {
+        type: "Feature",
+        "properties": {
+            Name:"Sakshi Varma",
+            Department:"Sales",
+            Addres:"Tiwasa, Yavatmal 445201, Maharashtra"},
+        "geometry": {
+          "coordinates": [
+            77.98422020723763,
+            20.37747818255245
+          ],
+          type: "Point"
+        }
+      }
+    ]
+  }
